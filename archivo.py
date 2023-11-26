5334676fd3ca9f1a674dc2ea046a0e52a49535ee
KarenReyesG <149857613+KarenReyesG@users.noreply.github.com>
2023-11-25 19:43:54 -0600

    archivo.py

--- archivo.py
+++ archivo.py
@@ -1,5 +1,6 @@
-num = 3
-cad = "PAulo"
+numero_entero = 5
+numero_flotante = 3.1416
+tipo_cadena = "Hola Mundo otra vez"
+tipo_bool = false
 
-print(num, cad)
-print("HOla")
+print(numero_entero, numero_flotante, tipo cadena, tipo bool)
